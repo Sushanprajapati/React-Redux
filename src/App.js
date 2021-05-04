aacbf92a41f855e58dcb8fe433767816a7f0b2b1
Sushan <sushanprajapati9808@gmail.com>
2021-05-04 12:57:54 +0545

    Playing with actions and Dispatch

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
   const counter= useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
       <h1>Counter {counter}</h1>
+
+      <button onClick={() => dispatch(increment())} className="b1">+</button>
+      <button onClick={()=> dispatch(decrement())}>-</button>
 
       {isLogged ? <h3>Valuable information shown only when isLogged is true </h3> : ''}
     </div>
